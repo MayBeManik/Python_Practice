9aa5801114aade4e4685494cd7e4bbe3f6a37324
Manik Kumar <109964479+MayBeManik@users.noreply.github.com>
2022-08-03 20:28:01 +0530

    Add files via upload

---
+++ Exercise Logical Operators.py
@@ -0,0 +1,10 @@
+is_magician = True
+is_expert = False
+if is_magician and is_expert:
+  print("You are a master magician")
+  
+elif is_magician and not(is_expert):
+  print("at least you are getting their")
+
+else:
+  print("you need magic powers")
